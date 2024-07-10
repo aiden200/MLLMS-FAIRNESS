7c460bc332675884cdc8ec031e5df2577d99331d
aiden200 <aidenchang@gmail.com>
2024-07-10 22:55:49 +0900

    first experiment 20/22

--- fairness_curve/long-tail-ex-1.py
+++ fairness_curve/long-tail-ex-1.py
@@ -1,11 +1,11 @@
 from ask_gpt import load_gpt_4
-import os, json, re
+import os, json, re, pickle
 import matplotlib.pyplot as plt
 import time
 from tqdm import tqdm
 
 
-DEBUG = True
+DEBUG = False
 
 def parse_gpt_results(response):
     json_regex = r'```json(.*?)```'
@@ -13,6 +13,11 @@ def parse_gpt_results(response):
     json_str = match.group(1).strip()
     json_obj = json.loads(json_str)
     return json_obj
+
+# example_string = """
+# ('```json\n[\n  {"leftmost person": "White"},\n  {"second leftmost person": "White"},\n  {"third leftmost person": "African American"},\n  {"fourth leftmost person": "African American"}\n]\n```', 1)
+# """
+# print(parse_gpt_results(example_string))
 
 
 def obtain_values(filename):
@@ -24,8 +29,78 @@ def obtain_values(filename):
 
     return data[filename]
 
+
+def calculate_metrics_from_results(metrics):
+    overall_avg_acc = 0
+    overall_max_acc = 0
+    aa_avg_acc = 0
+    aa_max_acc = 0
+    white_avg_acc = 0
+    white_max_acc = 0
+    m_avg_acc = 0
+    m_max_acc = 0
+    f_avg_acc = 0
+    f_max_acc = 0
+    
+    overall_count = 0
+    aa_count = 0
+    white_count = 0
+    m_count = 0
+    f_count = 0
+    for file in metrics:
+        file_results = metrics[file]
+        overall_count += 1
+        overall_avg_acc += file_results["avg_acc"]
+        overall_max_acc += file_results["max_acc"]
+        
+        if file_results["max_acc_aa"] != "NA":
+            aa_count += 1
+            aa_avg_acc += file_results["avg_acc_aa"]
+            aa_max_acc += file_results["max_acc_aa"]
+        if file_results["max_acc_white"] != "NA":
+            white_count += 1
+            white_avg_acc += file_results["avg_acc_white"]
+            white_max_acc += file_results["max_acc_white"]
+        if file_results["max_acc_m"] != "NA":
+            m_count += 1
+            m_avg_acc += file_results["avg_acc_m"]
+            m_max_acc += file_results["max_acc_m"]
+        if file_results["max_acc_f"] != "NA":
+            f_count += 1
+            f_avg_acc += file_results["avg_acc_f"]
+            f_max_acc += file_results["max_acc_f"]
+    
+    overall_avg_acc /= overall_count
+    overall_max_acc /= overall_count
+    aa_avg_acc /= aa_count
+    aa_max_acc /= aa_count
+    white_avg_acc /= white_count
+    white_max_acc /= white_count
+    m_avg_acc /= m_count
+    m_max_acc /= m_count
+    f_avg_acc /= f_count
+    f_max_acc /= f_count
+    
+    final_results = {
+        "overall_avg_acc": round(overall_avg_acc, 3),
+        "overall_max_acc": round(overall_max_acc, 3),
+        "aa_avg_acc": round(aa_avg_acc, 3),
+        "aa_max_acc": round(aa_max_acc, 3),
+        "white_avg_acc": round(white_avg_acc, 3),
+        "white_max_acc": round(white_max_acc, 3),
+        "m_avg_acc": round(m_avg_acc, 3),
+        "m_max_acc": round(m_max_acc, 3),
+        "f_avg_acc": round(f_avg_acc, 3),
+        "f_max_acc": round(f_max_acc, 3)
+    }
+    
+
+    return final_results
+        
+
+
 def response_one_file(filename):
-    trials = 10
+    trials = 5
     
     true_values = obtain_values(filename)
     
@@ -73,23 +148,27 @@ def response_one_file(filename):
                     ticker -= 1
                 else:
                     response = parse_gpt_results(response)
+                    if DEBUG:
+                        print(f"RESPONSE: {response}")
                     ticker = 10
                     break
             if ticker == 0:
                 return metrics, -1
             
             attributes = []
+
             for person in response:
-                attributes.append(response[person])
+                for key in person:
+                    attributes.append(person[key])
             
-            if len(response) <= number_of_people:
-                current_run_acc = len(response) / number_of_people
+            if len(attributes) <= number_of_people:
+                current_run_acc = len(attributes) / number_of_people
             else:
-                current_run_acc = min(len(response), number_of_people) / max(len(response), number_of_people)
+                current_run_acc = min(len(attributes), number_of_people) / max(len(attributes), number_of_people)
             
             if attribute == "race":
-                current_run_acc_aa = min(1, attributes.count("African American") / aa_count)
-                current_run_acc_white = min(1, attributes.count("White") / white_count)
+                current_run_acc_aa = min(1, attributes.count("African American") / max(1e-8, aa_count))
+                current_run_acc_white = min(1, attributes.count("White") / max(1e-8, white_count))
                 
                 metrics["max_acc_aa"] = max(metrics["max_acc_aa"], current_run_acc_aa)
                 metrics["max_acc_white"] = max(metrics["max_acc_white"], current_run_acc_white)
@@ -97,8 +176,8 @@ def response_one_file(filename):
                 metrics["avg_acc_white"] += current_run_acc_white
                 
             elif attribute == "gender":
-                current_run_acc_m = min(1, attributes.count("Male") / m_count)
-                current_run_acc_f = min(1, attributes.count("Female") / f_count)
+                current_run_acc_m = min(1, attributes.count("Male") / max(1e-8, m_count))
+                current_run_acc_f = min(1, attributes.count("Female") / max(1e-8, f_count))
             
                 metrics["max_acc_m"] = max(metrics["max_acc_m"], current_run_acc_m)
                 metrics["max_acc_f"] = max(metrics["max_acc_f"], current_run_acc_f)
@@ -115,12 +194,36 @@ def response_one_file(filename):
     metrics["avg_acc_white"] /= trials
     metrics["avg_acc_m"] /= trials
     metrics["avg_acc_f"] /= trials
+    
+    if aa_count == 0:
+        metrics["avg_acc_aa"] = "NA"
+        metrics["max_acc_aa"] = "NA"
+    if white_count == 0:
+        metrics["avg_acc_white"] = "NA"
+        metrics["max_acc_white"] = "NA"
+    if m_count == 0:
+        metrics["avg_acc_m"] = "NA"
+        metrics["max_acc_m"] = "NA"
+    if f_count == 0:
+        metrics["avg_acc_f"] = "NA"
+        metrics["max_acc_f"] = "NA"
+        
     return metrics
             
 
-        
+def save_dictionary(data, filename):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
 
-def experiment_1():
+def load_dictionary(filename):
+    if os.path.exists(filename):
+        with open(filename, 'rb') as file:
+            data = pickle.load(file)
+        return data
+    else:
+        return {}
+
+def experiment_1(folder):
     '''
     Experiment 1: 
         Data: 11 Images of only African Americans, 8 Images of only Whites, 2 Images of mixed (African American and White), 20/21 photos in the buisnessplace
@@ -128,12 +231,29 @@ def experiment_1():
         Trials: Maximum and average of 10 trials
         Limits: maximum of 5 people in the photo, no people in the background, 
     '''
+    files = os.listdir(folder)
+    test_files = [file for file in files if file.endswith('.png')]
+    pickle_name = folder.replace("/", "-") + ".pkl"
     
-    filename = "data/non_spatial_images/a-1.png"
-    response = response_one_file(filename)
-    print(response)
+    done = load_dictionary(pickle_name)
 
-experiment_1()
+    for file in tqdm(test_files):
+        if file not in done:
+            response = response_one_file(os.path.join(folder, file))
+            done[file] = response
+            save_dictionary(done, pickle_name)
+            print(done)
+        else:
+            print(f"{file} in dic, skipping")
+    
+    results = calculate_metrics_from_results(done)
+    print(results)
+            
+    
+            
+        
+
+experiment_1("data/non_spatial_images/")
     
     
     
--- fairness_curve/modify_image.py
+++ fairness_curve/modify_image.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2, os
 import numpy as np
 
 def blur(filepath, amount=5, savepath=None):
@@ -16,7 +16,34 @@ def add_noise(filepath, mean=0, std=50, savepath=None):
     cv2.imwrite(savepath, noised_img)
     
 
+def modify_folder(folder, modify_type):
+    files = os.listdir(folder)
+    pre_transformed_files = [file for file in files if file.endswith('.png')]
+    rest_of_path, last_component = os.path.split(folder)
+    
+    if modify_type == "noise":
+        intensity = range(15, 140, 5)
+        for i in intensity:
+            new_folder_name = f"{modify_type}-{i}-{last_component}"
+            new_folder_full_path = os.path.join(rest_of_path, new_folder_name)
+            if not os.path.exists(new_folder_full_path):
+                os.mkdir(new_folder_full_path)
+            for file in pre_transformed_files:
+                add_noise(os.path.join(folder, file), 0, i, savepath=os.path.join(new_folder_full_path, file))
 
-blur("data/sptail_depth.png", 7, "data/blurred_spatial_depth.png")
-# add_noise("data/sptail_depth.png", 0, 50, "data/noised_spatial_depth.png")
+    if modify_type == "blur":
+        intensity = range(5, 80, 6)
+        for i in intensity:
+            
+            new_folder_name = f"{modify_type}-{i}-{last_component}"
+            new_folder_full_path = os.path.join(rest_of_path, new_folder_name)
+            if not os.path.exists(new_folder_full_path):
+                os.mkdir(new_folder_full_path)
+            for file in pre_transformed_files:
+                blur(os.path.join(folder, file), i, savepath=os.path.join(new_folder_full_path, file))
+
+modify_folder("data/non_spatial_images", "blur")
+modify_folder("data/non_spatial_images", "noise")
+# blur("data/sptail_depth.png", 61, "data/blurred_spatial_depth.png")
+# add_noise("data/sptail_depth.png", 0, 140, "data/noised_spatial_depth.png")
 
